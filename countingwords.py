03af08b067fedcaf88f78e1286594049deafdf00
NidhiSrikanth <65636801+NidhiSrikanth@users.noreply.github.com>
2021-07-13 12:45:54 -0400

    Add files via upload

---
+++ countingwords.py
@@ -0,0 +1,11 @@
+introstring=input("enter your introduction");
+print(introstring)
+charactercount=0
+wordcount=1
+for i in introstring:
+
+    charactercount= charactercount+1
+    if(i==" "):
+        wordcount= wordcount+1  
+print(charactercount)
+print(wordcount)
---
+++ pocketmoney.py
@@ -0,0 +1,9 @@
+pocketmoney=int(input("enter the pocket money you get every month"))
+print(pocketmoney)
+
+if(pocketmoney> 500 ):
+    print(" you are a rich kid")
+elif(pocketmoney> 100):
+    print("your have a good life")
+else:
+    print("i know what you feel")
